859128cbf35404f3f5a009c3de948fd8c9807e43
Myles Palmer <myles@companion.studio>
2025-02-01 13:24:26 +0000

    NO Shift

--- web/src/components/modules/GridCarouselModule.svelte
+++ web/src/components/modules/GridCarouselModule.svelte
@@ -47,7 +47,7 @@
 	  // Remove 4px if there is only one item
 	  if (module.items.length === 1) {
 		console.log("Only one item");
-		currentPresetSize = `calc(${currentPresetSize} + 18px)`;
+		currentPresetSize = `calc(${currentPresetSize} + 24px)`;
 	  }
 
 	  if (module.items.length > 1) {
